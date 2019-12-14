dfae02d112b8a651a1fb374c699cb29d9e577fba
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-14 05:59:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,8 +48,8 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash.each
-holiday_hash.flatten
+  
+holiday_hash.value.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
