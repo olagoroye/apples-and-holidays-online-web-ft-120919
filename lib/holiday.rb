7958c463a330fa32779b18080701024a2131341b
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-14 05:29:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,7 +40,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-holiday_supplies[:spring][:memorial_day][2] = 
+holiday_supplies[:spring][:memorial_day][2] = "Table Cloth"
 return holiday_supplies
 end
 
