c9a496bc56e503325e4623f832d4d595059ef77c
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-14 06:42:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -34,8 +34,12 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  holiday_hash[:spring].each do |holiday, items|
+    items << supply
+    puts items
+  end
 
-holiday_supplies[:spring][:memorial_day][1] ="Table Cloth"
+# holiday_supplies[:spring][:memorial_day][1] ="Table Cloth"
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
