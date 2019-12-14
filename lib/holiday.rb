390920631f88a69ce1a5edd8e52adfdff72d0b62
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-14 06:17:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,14 +68,14 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+contacts.each do |holiday, item|
+  puts "#{holiday}: #{item}"
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.each do |person, data|
-  puts "#{person}: #{data}"
+  
 
 end
 
