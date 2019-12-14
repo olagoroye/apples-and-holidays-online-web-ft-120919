7ce86e63238def7379ccc4bfae96cf9aeeb2de38
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-14 06:29:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,10 +69,12 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 holiday_hash.each do |season, holiday|
   puts season.to_s.capitalize! << ":"
- holiday == "Christmas"
-   item.each do |attribute, supplies|
-     if attribue ==
-   
+ holiday.each do |holiday, supplies|
+      supplies = supplies.join(", ")
+      holiday = holiday.to_s.split("_").map {|w| w.capitalize}.join(" ")<< ":"
+      puts "  " + holiday + " " + supplies
+    end
+  end
    
 end
 
