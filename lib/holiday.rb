f2d7dc161100c31a21e9b7b5157f12e9af408832
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-14 05:13:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -26,7 +26,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
   holiday_supplies[:winter][:christmas][2] = "Balloons"
-  holiday_supplies[:winter][:new_year][1] = "Balloons"
+  holiday_supplies[:winter][:new_years][1] = "Balloons"
 
 end
 
